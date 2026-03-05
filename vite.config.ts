322c7762b4ec6e6d123053da44cb9f5641b58591
pzsantin <notpedroso@gmail.com>
2026-03-05 03:32:45 -0300

    desbug firebase 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? (process.env.VITE_BASE_URL || '/') : '/',
   server: {
     host: "::",
     port: 8080,
